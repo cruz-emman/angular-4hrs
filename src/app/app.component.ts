6d1de049db01db0d82941d6e2aaf67bfa13c89f7
cruz-emman <113968577+cruz-emman@users.noreply.github.com>
2024-09-16 22:45:53 +0800

    1:05:06 4hrs tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgIf, NgTemplateOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items: WishItem[] = [
+    // new WishItem('To Learn Angular'),
+    // new WishItem('Get Coffee', true),
+    // new WishItem("Find grass that cuts itself")
+  ];
+
+  newWishText = ""
   title = 'wishlist';
+
+  addNewWish(){
+    this.items.push(new WishItem(this.newWishText))
+    this.newWishText = ''
+  }
+  
+  toggleClick(item:WishItem){
+    item.isComplete = !item.isComplete
+    console.log(item)
+  }
 }
